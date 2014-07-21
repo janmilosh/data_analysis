c2ab829b2835d56dc1c24cc94208e93a2035d878
Jan Milosh <janmilosh@gmail.com>
2014-07-20 22:56:26 -0400

    Initial files

--- data_analysis/settings.py
+++ data_analysis/settings.py
@@ -57,8 +57,8 @@ WSGI_APPLICATION = 'data_analysis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':  'django.db.backends.postgresql_psycopg2',
+        'NAME': 'data_analysis',
     }
 }
 
