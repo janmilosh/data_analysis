4e02bf073bf2356564654fb938140a39a04da82b
Jan Milosh <janmilosh@gmail.com>
2014-07-21 23:29:07 -0400

    Slight addition to the model)

--- base_app/models.py
+++ base_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Location(models.Model):
     location = models.CharField(max_length=200)
     timestamp = models.DateTimeField('timestamp')
+
+    def was_saved_recently(self):
+        return self.timestamp >= timezone.now() - datetime.timedelta(days=3)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.location
