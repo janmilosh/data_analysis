cb42375ef02bbe80e8120eeed3d8e43c10f7e63a
Jan Milosh <janmilosh@gmail.com>
2014-07-28 21:27:12 -0400

    Adds Twitter API constants and start of twitter api call in views.py

--- base_app/views.py
+++ base_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import twitter
 
 def index(request):
     return HttpResponse("Hello, world. You're at the base_app index.")
+
+def getTweets():
+    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                           CONSUMER_KEY, CONSUMER_SECRET)
+
+    twitter_api = twitter.Twitter(auth=auth)
