965e7109d7120c9849aa2eb67a5dfb442b563d15
Jan Milosh <janmilosh@gmail.com>
2014-09-20 13:29:14 -0400

    Start of getting line plot working.
    - data is now in csv file.
    - still no plot, but bringing data into template.

--- stocks/views.py
+++ stocks/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
-import json
 import requests
-from pandas.io.data import DataReader
-from pandas import DataFrame, Series
 from datetime import datetime
 from pandas import DatetimeIndex
 
@@ -19,25 +16,18 @@ def stocks(request):
 
 def stock(request, stock_id=1):
     stock = get_object_or_404(Stock, id=stock_id)
+    print stock.ticker
 
-    data = get_stock_data()
-
-    
+    data = get_stock_data(stock.ticker)
 
     return render(request, 'stock.html', ({
         'stock': stock,
         'stock_data': data,
     }))
 
-def get_stock_data():
-    data = DataReader('aapl', 'yahoo', start = '01-01-2014')
-
-    high_json = data['High'].to_json(date_format='iso')
-    
-    return high_json
-
-
-# data comes from yahoo api
-# convert data into format for graphs (json object)
-# return json to template for graphs
-# need to hook up vega and D3
+def get_stock_data(stock):
+    stock = 'GSPC'
+    request_string = 'http://ichart.finance.yahoo.com/table.csv?s=^'+ stock + '%20&a=00&b=01&c=2010&g=d'
+    print request_string
+    data = requests.get(request_string)
+    return data.content
